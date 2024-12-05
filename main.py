3b654dab938bad54e7cf858cc15e6f5782bc43a5
Adrian Ditmansen <adriandenutrolige@gmail.com>
2024-12-05 23:11:41 +0100

    laget box funksjon

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ class Box:
     def take(self,player):
         if player == True:
             self.status = 1
+            self.draw = "|X|"
         elif player == False:
             self.status = 2
+            self.draw = "|O|"
 
-    
-    def move(self):
+    def move(self,player):
         if self.boxtaken() == False:
-            self.take()
-        
-
+            self.take(player)
+        elif self.boxtaken() == True:
+            print("velg en ny box")
 
 box1=Box()
 box2=Box()
@@ -32,4 +33,8 @@ box6=Box()
 box7=Box()
 box8=Box()
 box9=Box()
+
+def showBoard():
+    print("\n",box1.draw + box2.draw + box3.draw,"\n",box4.draw + box5.draw + box6.draw,"\n",box7.draw + box8.draw + box9.draw)
+
 
